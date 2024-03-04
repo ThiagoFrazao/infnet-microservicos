489a91f6c395c9c0a9dc5d27de176df1e3905ea7
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2024-03-04 20:53:04 -0300

    feat: iniciando criacao dos projetos

---
+++ notification/src/test/java/br/edu/infnet/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ products/src/test/java/br/edu/infnet/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
