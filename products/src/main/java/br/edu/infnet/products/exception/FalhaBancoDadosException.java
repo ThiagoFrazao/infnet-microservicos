fa3fe67315a4b605f99f311b5c8ecc5fe62d3567
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2024-03-07 21:32:27 -0300

    feat: adicionando exception

---
+++ products/src/main/java/br/edu/infnet/products/exception/FalhaBancoDadosException.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.products.exception;
+
+public class FalhaBancoDadosException extends RuntimeException {
+
+    public FalhaBancoDadosException(String mensagem) {
+        super(mensagem);
+    }
+
+}
