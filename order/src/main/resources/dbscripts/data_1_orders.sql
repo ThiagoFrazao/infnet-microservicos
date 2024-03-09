ebb286ec8334cab6283566d7b95b0cfe18dc5357
Thiago da Costa Pinto Frazao de Sousa <f514189@corp.caixa.gov.br>
2024-03-08 21:59:12 -0300

    feat: finalizada criacao do Order e comunicacao com Produtos

--- order/src/main/resources/dbscripts/data_1_orders.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "usuario" ("nome", "senha", "email", "role") VALUES ('Thiago Frazao', '123456', 'thiago.frazao@teste.ok', 'ADMIN');
-INSERT INTO "usuario" ("nome", "senha", "email", "role")  VALUES ('Godofredo da Silva', 'godofredosilva123@gmail.com', '456789', 'CONSUMER');
